d971bb6b86ce1107d829fc6726d4926443c2e724
Choi Jiwon <winning1993@naver.com>
2020-05-08 07:49:03 +0900

    랜덤 금액 생성

--- index.js
+++ index.js
@@ -14,16 +14,17 @@ function handleAdd(){
     const lastNames = ["민준","서준","예준","도윤","시우","주원","하준","지호","지후",
 "준서","준우","현우","지훈","도현"];
     const randomNum = Math.floor(Math.random()*24);
-    const firstName = firstNames.split("")[randomNum];
     const randomNum2 = Math.floor(Math.random()*13);
+    const randomWealth = `${Math.floor(Math.random()*100000000)}원`;
+    const firstName = firstNames.split("")[randomNum];
     const lastName = lastNames[randomNum2];
-    console.log(randomNum)
-    console.log(randomNum2);
     const fullName = firstName+lastName;
     span.innerHTML = fullName;
     div.appendChild(span);
-
     personArea.appendChild(div);
+    span2.innerHTML = randomWealth;
+    div.appendChild(span2);
+    div.className = "person_info";
 }
 
 function init(){
